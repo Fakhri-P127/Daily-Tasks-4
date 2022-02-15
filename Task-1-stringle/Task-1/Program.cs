efde2ec0da5061c32385ac9c3691d049ee27e123
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-15 22:17:13 +0400

    added Task-1-stringle folder

---
+++ Task-1-stringle/Task-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exampleTxt =  "Hello how are yOu I'm FRom P127. Siz tekmiLlesHdIrilmeshleRdensiniZmi. Yox biZ iNTeRnoSionaLLashdirilmishLaRDaniq" ;
+            
+            ChangeCase(exampleTxt);
+        }
+        public static void ChangeCase(string str )
+        {           
+                foreach (char letter in str)
+                {
+                    if (char.IsLower(letter))
+                    {
+                        Console.Write(char.ToUpper(letter));
+                    }
+                    else
+                    {
+                        Console.Write(char.ToLower(letter));
+                    }
+                }
+            
+        }
+    }
+}
