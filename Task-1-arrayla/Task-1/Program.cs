be46607928115243aa2d7e7568cb1b01000986ec
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-15 22:20:43 +0400

    added Task-1-arrayla
    git commit -m added

---
+++ Task-1-arrayla/Task-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] exampleArr = { "Hello how are yOu I'm FRom P127. Siz tekmiLlesHdIrilmeshleRdensiniZmi. Yox biZ iNTeRnoSionaLLashdirilmishLaRDaniq" } ;
+            
+            ChangeCase(exampleArr);
+        }
+        public static void ChangeCase(string[] arr )
+        {
+            foreach (string str in arr)
+            {
+                foreach (char letter in str)
+                {
+                    if (char.IsLower(letter))
+                    {
+                        Console.Write(char.ToUpper(letter));
+                    }
+                    else
+                    {
+                        Console.Write(char.ToLower(letter));
+                    }
+                }
+            }
+        }
+    }
+}
