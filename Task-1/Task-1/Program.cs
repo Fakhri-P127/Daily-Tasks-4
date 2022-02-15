ad210a417f05e807a517e2327ef1abf5fb801691
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-15 15:34:46 +0400

    added Task-1 folder

---
+++ Task-1/Task-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "Hello how are yOu I'm from P127. Siz tekmiLlesdIrilmesleRdensiniZmi" };
+            
+            Change(ref arr);
+                        
+        }
+        public static void Change(ref string[] arr)
+        {
+            
+            foreach (string text in arr)
+            {
+                foreach (char letter in text)
+                {
+                    if (char.IsLower(letter))
+                    {
+                        Console.Write(char.ToUpper(letter));                        
+                    }
+                    else
+                    {
+                        Console.Write(char.ToLower(letter));                        
+                    }
+                }                                   
+            }
+        }
+    }
+}
